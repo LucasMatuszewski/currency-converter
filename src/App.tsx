571bae73dbe05a0a61ea320954e1ff2bc7461f59
Łukasz Matuszewski <lucas.matuszewski@gmail.com>
2020-10-31 19:13:03 +0100

    Add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
+import Converter from './pages/Converter/Converter';
+import NotFound from './pages/NotFound/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Currency Converter</h1>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path={['/', 'currency/:baseCurrency']}>
+          <Converter />
+        </Route>
+        <Route path={'/not-found'}>
+          <NotFound />
+        </Route>
+        <Route path="*">
+          <Redirect to="/not-found" />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Converter/Converter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Converter() {
+  return (
+    <div>
+      <h1>Currency Converter</h1>
+    </div>
+  );
+}
+
+export default Converter;
