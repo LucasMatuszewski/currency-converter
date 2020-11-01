3a0fdf2b94e0456265809ac278904bc01c9ad323
Łukasz Matuszewski <lucas.matuszewski@gmail.com>
2020-11-01 12:41:43 +0100

    Add NotFound page content & styles

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import Layout from '../../components/Layout/Layout';
+import './notFound.scss';
 
 function NotFound() {
   return (
     <Layout title="Page Not Found - Currency Converter">
-      <h1>Page Not Found</h1>
+      <div className="box has-text-centered not-found-container">
+        <h1 className="has-text-info">OOPS!</h1>
+        <h2 className="has-text-weight-semibold">
+          We can't find the page you're looking for.
+        </h2>
+        <Link
+          className="button is-info has-text-weight-semibold is-outlined"
+          to="/"
+        >
+          Visit homepage
+        </Link>
+      </div>
     </Layout>
   );
 }
